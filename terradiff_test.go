74245d7c8f201c6049815a20578cdc8e4ec46c97
paper2 <35333687+paper2@users.noreply.github.com>
2023-11-05 11:38:03 +0900

    change tests

--- terradiff_test.go
+++ terradiff_test.go
@@ -11,30 +11,37 @@ import (
 func TestTerradiff_Compare(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
-		name      string
-		srcBranch string
-		dstBranch string
-		repoURL   string
-		isEqual   bool
+		name            string
+		repoURL         string
+		srcBranch       string
+		terraformSrcDir string
+		dstBranch       string
+		terraformDstDir string
+		isEqual         bool
 	}{
 		{
-			name:      "compare branches having diffelent changes",
-			srcBranch: "branch1",
-			dstBranch: "main",
-			repoURL:   "https://github.com/paper2/test-terradiff",
-			isEqual:   false,
+			name:            "compare branches having diffelent changes",
+			repoURL:         "https://github.com/paper2/terradiff",
+			srcBranch:       "main",
+			terraformSrcDir: "resource/test/terraform/src",
+			dstBranch:       "main",
+			terraformDstDir: "resource/test/terraform/dst",
+			isEqual:         false,
 		},
 		{
-			name:      "compare same branches",
-			srcBranch: "main",
-			dstBranch: "main",
-			repoURL:   "https://github.com/paper2/test-terradiff",
-			isEqual:   true,
+			name:            "compare branches having same changes",
+			repoURL:         "https://github.com/paper2/terradiff",
+			srcBranch:       "main",
+			terraformSrcDir: "resource/test/terraform/dst",
+			dstBranch:       "main",
+			terraformDstDir: "resource/test/terraform/dst",
+			isEqual:         true,
 		},
 	}
 
 	for _, tc := range tests {
 		tc := tc
+		// TODO: terradiff関数を直接使って作成されたjsonの内容を確認するテストにする
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			workDir := t.TempDir()
@@ -44,8 +51,10 @@ func TestTerradiff_Compare(t *testing.T) {
 			srcGit := NewGit(NewCommandExecutor("."), tc.repoURL, srcDir, tc.srcBranch)
 			dstGit := NewGit(NewCommandExecutor("."), tc.repoURL, dstDir, tc.dstBranch)
 
-			srcTerraform := NewTerraform(NewCommandExecutor(srcDir))
-			dstTerrafom := NewTerraform(NewCommandExecutor(dstDir))
+			srcTerraformDir := filepath.Join(srcDir, tc.terraformSrcDir)
+			dstTerraformDir := filepath.Join(dstDir, tc.terraformDstDir)
+			srcTerraform := NewTerraform(NewCommandExecutor(srcTerraformDir))
+			dstTerrafom := NewTerraform(NewCommandExecutor(dstTerraformDir))
 
 			terradiff := NewTerradiff(
 				Operators{tf: srcTerraform, git: srcGit},
